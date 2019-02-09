0c05d2edcbe41dfe3218e0c45123dd211380d18b
tuple66 <djwhite66@gmail.com>
2019-02-09 21:05:55 +0900

    html 동적표현 방법

--- WhiteNode/app.js
+++ WhiteNode/app.js
@@ -11,7 +11,30 @@ app.get('/route', function(req, res){
     res.send('hello route <img src="/route.png">');
   });
 
-  
+  app.get('/dynamic', function(req, res){
+    var str='';
+    for(var i=1;i<=5;i++){
+        str=str+'<li>cording</li>';
+    }
+
+    var output =`
+    <html>
+    <head>
+        <meta charset="utf-8">
+        <title> html 동적파일  
+        </title>
+    </head>
+    <body>
+        <h1>Hello,dynamic</h1>
+        <ul>
+            ${str}
+        </ul>
+    </body>
+    </html>
+    `;  
+    res.send(output);
+  });
+
 app.get('/login', function(req, res){
     res.send('<h1>login please</h1>');
   });
