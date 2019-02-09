7a299135fd0c920fb010094f72f828ef3d8a8b61
tuple66 <djwhite66@gmail.com>
2019-02-09 20:50:31 +0900

    정적파일서비스

--- WhiteNode/app.js
+++ WhiteNode/app.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+
 app.get('/', function(req, res){
   res.send('hello world');
 });
 
+app.get('/route', function(req, res){
+    res.send('hello route <img src="/route.png">');
+  });
+
+  
 app.get('/login', function(req, res){
     res.send('<h1>login please</h1>');
   });
