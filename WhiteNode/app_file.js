e0a00570df7afc0daaaa183727899a68b9678e6c
tuple66 <djwhite66@gmail.com>
2019-02-10 18:34:40 +0900

    파일목록을 읽어서 링크를 만든다.

--- WhiteNode/app_file.js
+++ WhiteNode/app_file.js
@@ -22,15 +22,21 @@ app.post('/topic',function(req,res){
 
     fs.writeFile('data/'+title,description,(err)=>{
         if(err){
-            console.log(err);
-            res.status(500).send('Internal Server Error');
+
         }
         res.send('Success!!!');
     });
-
-    
 });
 
-
+app.get('/topic',function(req,res){
+    fs.readdir('data',function(err,files){
+        if(err){
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        }
+        res.render('view_file',{topics:files});
+    });
+    
+});
 
 app.listen(3000);
